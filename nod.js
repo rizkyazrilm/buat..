79752fd2e621a190bd66e0477f5245ed19b61406
Azril Muhamad Rizki <126126125+rizkyazrilm@users.noreply.github.com>
2023-08-30 19:56:44 +0700

    Update nod.js

--- nod.js
+++ nod.js
@@ -15,3 +15,11 @@ const createWord = (text, index) => {
 const addWord = (text, index) => subtitle.appendChild(createWord(text, index));
 const createSubtitle = text => text.split("").map(addWord);
 createSubtitle("happy  birthday yeuwww,  you're  17  now  hewhwehewi wish all the better  things will came in to your life.you've been doing great       last year, soo keep itupp!! and be the best versionof yourself okkayy!!! ");
+
+var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+if (!isChrome){
+    $('#iframeAudio').remove()
+}
+else {
+    $('#playAudio').remove() // just to make sure that it will not have 2x audio in the background 
+}
